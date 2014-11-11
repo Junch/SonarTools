29ad30de59a3db4c1e41e40b320db045b3621c47
Jun Chen <junc76@gmail.com>
2014-11-11 17:59:46 +0800

    Add the projects SonarTools and SonarTools.Test

---
+++ SonarTools.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarTools.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void Generate_Setting_From_PropertiesValue() {
+            RunnerConfig config = new RunnerConfig() {
+                ProjectName = "Simple example",
+                SourceEncoding = "UTF-8",
+                Language = "c++"};
+
+            var setting = config.GetSettings();
+
+            Assert.AreEqual(3, setting.Count);
+
+            Assert.AreEqual("-Dsonar.projectName=Simple example", setting[0]);
+            Assert.AreEqual("-Dsonar.language=c++", setting[1]);
+            Assert.AreEqual("-Dsonar.sourceEncoding=UTF-8", setting[2]);
+        }
+    }
+}
---
+++ SonarTools/RunnerConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarTools
+{
+    public class RunnerConfig {
+        public String ProjectKey { get; set; }
+        public String ProjectName { get; set; }
+        public String ProjectVersion { get; set; }
+        public String Language { get; set; }
+        public String SourceEncoding { get; set; }
+
+        public String Include { get; set; }
+        public String Sources { get; set; }
+
+        public List<String> GetSettings () {
+            var type = typeof(RunnerConfig);
+            PropertyInfo[] pi= type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+
+            List<String> setting = new List<string>();
+
+            foreach (var prop in pi) {
+                String value = prop.GetValue(this) as String;
+                if (String.IsNullOrEmpty(value))
+                    continue;
+
+                String propName = FirstLetterLowercase(prop.Name);
+
+                setting.Add(String.Format("-Dsonar.{0}={1}", propName, value));
+            }
+
+            return setting;
+        }
+
+        private static String FirstLetterLowercase(String propName) {
+            return Char.ToLower(propName[0]) + propName.Substring(1);
+        }
+    }
+}
