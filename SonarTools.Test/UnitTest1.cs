7a5eea7d01306bd2ac40d568b36690c37fbd59ae
Jun Chen <junc76@gmail.com>
2014-11-13 10:38:40 +0800

    Use indexer to add additional properties

--- SonarTools.Test/UnitTest1.cs
+++ SonarTools.Test/UnitTest1.cs
@@ -5,19 +5,34 @@ namespace SonarTools.Test {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void Generate_Setting_From_PropertiesValue() {
-            RunnerConfig config = new RunnerConfig() {
+        public void Generate_Setting_From_Properties() {
+            RunnerConfig config = new RunnerConfig {
                 ProjectName = "Simple example",
                 SourceEncoding = "UTF-8",
                 Language = "c++"};
-
             var setting = config.GetSettings();
+            setting.Sort();
 
             Assert.AreEqual(3, setting.Count);
 
-            Assert.AreEqual("-Dsonar.projectName=Simple example", setting[0]);
-            Assert.AreEqual("-Dsonar.language=c++", setting[1]);
+            Assert.AreEqual("-Dsonar.language=c++", setting[0]);
+            Assert.AreEqual("-Dsonar.projectName=Simple example", setting[1]);
             Assert.AreEqual("-Dsonar.sourceEncoding=UTF-8", setting[2]);
+        }
+
+        [TestMethod]
+        public void Generate_Setting_From_AdditionalProperties() {
+            RunnerConfig config = new RunnerConfig {
+                SourceEncoding = "UTF-8"
+            };
+            config["cxx.cppcheck.reportPath"] = "cppcheck_coredll.xml";
+
+            var setting = config.GetSettings();
+            setting.Sort();
+
+            Assert.AreEqual(2, setting.Count);
+            Assert.AreEqual("-Dsonar.cxx.cppcheck.reportPath=cppcheck_coredll.xml", setting[0]);
+            Assert.AreEqual("-Dsonar.sourceEncoding=UTF-8", setting[1]);
         }
     }
 }
--- SonarTools/RunnerConfig.cs
+++ SonarTools/RunnerConfig.cs
@@ -10,12 +10,25 @@ namespace SonarTools
     public class RunnerConfig {
         public String ProjectKey { get; set; }
         public String ProjectName { get; set; }
+        public String ProjectDescription { get; set; }
         public String ProjectVersion { get; set; }
         public String Language { get; set; }
         public String SourceEncoding { get; set; }
 
         public String Include { get; set; }
         public String Sources { get; set; }
+
+        private Dictionary<String, String> additionProperties = new Dictionary<String, String>();
+        
+        public String this [String prop]{
+            set {
+                additionProperties[prop] = value;
+            }
+
+            get {
+                return additionProperties[prop];
+            }
+        }
 
         public List<String> GetSettings () {
             var type = typeof(RunnerConfig);
@@ -24,6 +37,9 @@ namespace SonarTools
             List<String> setting = new List<string>();
 
             foreach (var prop in pi) {
+                if (prop.GetIndexParameters().Length > 0)
+                    continue;
+
                 String value = prop.GetValue(this) as String;
                 if (String.IsNullOrEmpty(value))
                     continue;
@@ -31,6 +47,14 @@ namespace SonarTools
                 String propName = FirstLetterLowercase(prop.Name);
 
                 setting.Add(String.Format("-Dsonar.{0}={1}", propName, value));
+            }
+
+            foreach (var pair in additionProperties) {
+                if (String.IsNullOrEmpty(pair.Value))
+                    continue;
+
+                String propName = FirstLetterLowercase(pair.Key);
+                setting.Add(String.Format("-Dsonar.{0}={1}", propName, pair.Value));
             }
 
             return setting;
