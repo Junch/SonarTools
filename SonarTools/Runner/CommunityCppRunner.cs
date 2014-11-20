4656998b77c3b205c29cf5a284f4da226c80fed6
Jun Chen <junc76@gmail.com>
2014-11-20 11:07:34 +0800

    Refactor the method RunCppCheckCmd

--- SonarTools/Runner/CommunityCppRunner.cs
+++ SonarTools/Runner/CommunityCppRunner.cs
@@ -16,7 +16,12 @@ namespace SonarTools.Runner {
             this["ProjectBaseDir"] = DirectoryName;
             this["cxx.cppcheck.reportPath"] = ProjectKey + ".xml";
             this["cxx.suffixes.headers"]= ".x"; // Don't parse the header files as the commerical does
-            cppcheckWriter = File.CreateText(DirectoryName + "\\" + ProjectKey + ".xml");
+        }
+
+        public String CppcheckLogFilepath {
+            get {
+                return String.Format("{0}\\{1}.xml", DirectoryName, ProjectKey);
+            }
         }
 
         public String CppCheckCmdArguments {
@@ -26,30 +31,29 @@ namespace SonarTools.Runner {
         }
 
         public void RunCppCheckCmd() {
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = "cppcheck.exe";
-            psi.Arguments = CppCheckCmdArguments;
-            psi.UseShellExecute = false;
-            psi.RedirectStandardOutput = true;
-            psi.RedirectStandardError = true;
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-            psi.CreateNoWindow = true;
-            psi.ErrorDialog = false;
-            psi.WorkingDirectory = DirectoryName;
+            using (cppcheckWriter = File.CreateText(CppcheckLogFilepath)) {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "cppcheck.exe";
+                psi.Arguments = CppCheckCmdArguments;
+                psi.UseShellExecute = false;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                psi.CreateNoWindow = true;
+                psi.ErrorDialog = false;
+                psi.WorkingDirectory = DirectoryName;
 
-            using (Process proc = Process.Start(psi)) {
-                proc.OutputDataReceived += proc_DataReceived;
-                proc.ErrorDataReceived += proc_ErrorDataReceived;
-                proc.EnableRaisingEvents = true;
+                using (Process proc = Process.Start(psi)) {
+                    proc.OutputDataReceived += proc_DataReceived;
+                    proc.ErrorDataReceived += proc_ErrorDataReceived;
+                    proc.EnableRaisingEvents = true;
 
-                proc.Start();
-                proc.BeginErrorReadLine();
-                proc.BeginOutputReadLine();
-                proc.WaitForExit();
+                    proc.Start();
+                    proc.BeginErrorReadLine();
+                    proc.BeginOutputReadLine();
+                    proc.WaitForExit();
+                }
             }
-
-            if (cppcheckWriter != null)
-                cppcheckWriter.Close();
         }
 
         void proc_DataReceived(object sender, DataReceivedEventArgs e) {
