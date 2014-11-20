f13f9bcf118d209914263a6450cd947e418abe13
Jun Chen <junc76@gmail.com>
2014-11-20 17:10:19 +0800

    Refactor the method ParserManager.Parse
    Add try catch to prevent one exceptions crash the whole application

--- SonarTools/ParserManager.cs
+++ SonarTools/ParserManager.cs
@@ -20,35 +20,45 @@ namespace SonarTools {
         public String SonarRunnerHome { get; set; }
 
         public SonarRunner Parser(String projectPath) {
-            Project proj = new Project(projectPath);
+            try {
+                Project proj = new Project(projectPath);
 
-            ProjectProperty prop = proj.GetProperty("Language");
-            if (prop.EvaluatedValue == "C++") {
-
-                VcxprojParser parser = new VcxprojParser(proj);
-                IEnumerable<String> includes = parser.IncludeDirectories;
-                System.Diagnostics.Debug.Assert(includes.Count() > 0);
-                if (includes.Count() == 0) {
-                    System.Console.WriteLine("Failed to get the include directories");
+                ProjectProperty prop = proj.GetProperty("Language");
+                if (prop.EvaluatedValue == "C++") {
+                    return ParseCpp(proj);
                 }
-                var includesWithQuates = from x in includes select String.Format("\"{0}\"",x);
-                String includePaths = String.Join(",", includesWithQuates);
-
-                SonarRunner runner;
-                if (cppPlugType == PluginType.kCppCommercial) {
-                     runner = new CommercialCppRunner(projectPath, Branch);
-                     runner["cfamily.library.directories"] = includePaths;
-                } else {
-                    runner = new CommunityCppRunner(projectPath, Branch);
-                    //runner["cxx.include_directories"] = includePaths; // For V0.9.0
-                    runner["cxx.includeDirectories"] = includePaths; // For V0.9.1
-                }
-
-                AddGeneralSetting(runner);
-                return runner;
+            }
+            catch (Exception e){
+                Console.WriteLine("Exception Catch: {0}\n", e.Message);
             }
 
             return null;
+        }
+
+        private SonarRunner ParseCpp(Project proj) {
+            String projectPath = proj.FullPath;
+
+            VcxprojParser parser = new VcxprojParser(proj);
+            IEnumerable<String> includes = parser.IncludeDirectories;
+            if (includes.Count() == 0) {
+                throw new InvalidOperationException(String.Format("Failed to get the include directories from {0}", projectPath));
+            }
+            
+            var includesWithQuates = from x in includes select String.Format("\"{0}\"", x);
+            String includePaths = String.Join(",", includesWithQuates);
+
+            SonarRunner runner;
+            if (cppPlugType == PluginType.kCppCommercial) {
+                runner = new CommercialCppRunner(projectPath, Branch);
+                runner["cfamily.library.directories"] = includePaths;
+            } else {
+                runner = new CommunityCppRunner(projectPath, Branch);
+                //runner["cxx.include_directories"] = includePaths; // For V0.9.0
+                runner["cxx.includeDirectories"] = includePaths; // For V0.9.1
+            }
+
+            AddGeneralSetting(runner);
+            return runner;
         }
 
         private void AddGeneralSetting(SonarRunner runner) {
